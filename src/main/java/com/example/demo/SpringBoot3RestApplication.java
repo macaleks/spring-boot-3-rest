b55c5a08ce283372228899ddb0f68460b9c7d490
macaleks <31843119+macaleks@users.noreply.github.com>
2025-12-05 09:42:25 +0300

    Add controller test with mocked service (#1)
    simple app

---
+++ src/main/java/com/example/demo/SpringBoot3RestApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot3RestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot3RestApplication.class, args);
+    }
+}
